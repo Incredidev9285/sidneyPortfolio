e0de1be617c51d1d4ec83ed0f0e3f25e8f055b29
developer <jamesjinn9285@gmail.com>
2025-06-05 15:56:07 -0300

    second

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -23,7 +23,7 @@ function About() {
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
               KnoW Who <strong className="purple">I'M</strong>
-            </h1>sss
+            </h1>
             <Aboutcard />
           </Col>
           <Col
